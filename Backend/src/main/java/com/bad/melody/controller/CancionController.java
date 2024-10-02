675bf61bd02090759633b14c9807f213e3320db6
Bryam777 <164650497+Bryam777@users.noreply.github.com>
2024-10-02 17:36:17 -0500

    actualizacion
    controller de cancion para guardar

--- Backend/src/main/java/com/bad/melody/controller/CancionController.java
+++ Backend/src/main/java/com/bad/melody/controller/CancionController.java
@@ -3,14 +3,18 @@ package com.bad.melody.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bad.melody.model.Cancion;
-import com.bad.melody.services.impl.CancionServiceImpl;;
+import com.bad.melody.services.impl.CancionServiceImpl;
 
 @RestController
 @RequestMapping("/api/canciones")
@@ -21,7 +25,7 @@ public class CancionController {
 
     @DeleteMapping("/{id}")
     public String eliminarCancionPorId (@PathVariable("id") Long id){
-        boolean verificar = this.cancionServiceImpl.eliminarCancion(id);
+        boolean verificar = cancionServiceImpl.eliminarCancion(id);
         if (verificar) {
             return "La cancion se elimino exitosamente";
         }else{
@@ -34,5 +38,13 @@ public class CancionController {
         return cancionServiceImpl.obtenerLasCanciones();
     }
 
-    
+    @PostMapping("/ingresar-cancion")
+    public ResponseEntity<Cancion> ingresarCancion(@RequestBody Cancion subirCancion){
+        try {
+            Cancion nuevaCancion = cancionServiceImpl.guardarCancion(subirCancion);
+            return ResponseEntity.status(HttpStatus.CREATED).body(nuevaCancion);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
 }
--- Backend/src/main/java/com/bad/melody/services/ComentarioCancionService.java
+++ Backend/src/main/java/com/bad/melody/services/ComentarioCancionService.java
@@ -1,6 +1,11 @@
 package com.bad.melody.services;
 
+import com.bad.melody.model.Cancion;
+import com.bad.melody.model.Usuario;
+
 public interface ComentarioCancionService {
 
-    
+    void calificacionCancion (Cancion cancionId, Usuario usuarioId, int calificacion);
+
+    int obtenerPromedioCalificacion (Long cancionId);
 }
